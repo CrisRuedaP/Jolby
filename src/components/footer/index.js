61c9816ca458a3db0294608995a2ea1bfd82e7a8
Cristina Rueda <crisruedap@gmail.com>
2020-10-19 19:05:21 -0500

    links created in the footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -7,7 +7,6 @@ import Icon_copyright from "../../assets/images/copyright.png";
 import Icon_heart from "../../assets/images/heart.png";
 import Icon_marker from "../../assets/images/marker.png";
 
-
 const Footer = () => {
   return (
     <div className="footer">
@@ -15,7 +14,8 @@ const Footer = () => {
         <Nav className="justify-content-center" activeKey="/home">
           <p className="text-center mt-4 mb-4">
             Jolby By Coderise
-            <br /><img className="footer__up-icon" src={Icon_marker} alt={"icon"}/>
+            <br />
+            <img className="footer__up-icon" src={Icon_marker} alt={"icon"} />
             Medellin, Colombia
           </p>
         </Nav>
@@ -24,13 +24,53 @@ const Footer = () => {
         <Container>
           <Navbar.Brand href="#home">
             <span className="dark small">
-                <img className="footer__icon-left" src={Icon_copyright} alt={"icon"} />
-             Illustration Absurd Design</span>
+              <img
+                className="footer__icon-left"
+                src={Icon_copyright}
+                alt={"icon"}
+              />
+              <a
+                href="https://absurd.design/"
+                target="_blank"
+                className="footer-link"
+              >
+                Illustration Absurd Design
+              </a>
+            </span>
           </Navbar.Brand>
           <Nav className="justify-content-end" activeKey="/home">
             <Nav.Item>
               <p className="dark small">
-                Made with  <img className="footer__icon-right" src={Icon_heart} alt={"icon"} /> by Cristina Rueda - Marisol Ramírez - David Hincapié
+                Made with{" "}
+                <img
+                  className="footer__icon-right"
+                  src={Icon_heart}
+                  alt={"icon"}
+                />{" "}
+                by{" "}
+                <a
+                  href="https://twitter.com/CrisRuedaP"
+                  className="footer-link"
+                  target="_blank"
+                >
+                  Cristina Rueda
+                </a>{" "}
+                -{" "}
+                <a
+                  href="https://twitter.com/MarisolRHe"
+                  className="footer-link"
+                  target="_blank"
+                >
+                  Marisol Ramírez
+                </a>{" "}
+                -{" "}
+                <a
+                  href="https://twitter.com/dalejohgi"
+                  className="footer-link"
+                  target="_blank"
+                >
+                  David Hincapié
+                </a>
               </p>
             </Nav.Item>
           </Nav>
